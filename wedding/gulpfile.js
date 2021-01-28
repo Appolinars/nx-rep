d06ea31af8a6a148f25c5f0c186061676f8f335d
appolinaris <vakulenko.maksim977@gmail.com>
2021-01-29 01:19:26 +0200

    fix

--- wedding/gulpfile.js
+++ wedding/gulpfile.js
@@ -40,19 +40,19 @@ async function iconfont() {
 
 function images() {
     return src('app/images/**/*')
-        // .pipe(imagemin(
-        //     [
-        //         imagemin.gifsicle({ interlaced: true }),
-        //         imagemin.mozjpeg({ quality: 75, progressive: true }),
-        //         imagemin.optipng({ optimizationLevel: 5 }),
-        //         imagemin.svgo({
-        //             plugins: [
-        //                 { removeViewBox: true },
-        //                 { cleanupIDs: false }
-        //             ]
-        //         })
-        //     ]
-        // ))
+        .pipe(imagemin(
+            [
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.mozjpeg({ quality: 75, progressive: true }),
+                imagemin.optipng({ optimizationLevel: 5 }),
+                imagemin.svgo({
+                    plugins: [
+                        { removeViewBox: true },
+                        { cleanupIDs: false }
+                    ]
+                })
+            ]
+        ))
         .pipe(dest('dist/images'))
 }
 
